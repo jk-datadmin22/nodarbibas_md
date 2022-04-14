64e4f0a41be8c91a234b3315f9239f95bc511a33
Maris Danne <maris.danne@gmail.com>
2022-04-14 19:40:07 +0300

    first

---
+++ main.py
@@ -0,0 +1,49 @@
+print("Sveika, pasaule!")
+print("Rīt būs brīvdiena!")
+print()
+print()
+
+print("Teksts zemāk!")
+print("piens kefirs sviests biezpiens krejums")
+print("piens kefirs", "sviests", "biezpiens", "krejums")
+print("piens kefirs", "sviests", "biezpiens", "krejums", sep="#")
+print()
+print()
+
+print("bērzs, osis, egle, priede")
+print("bērzs", "osis", "egle", "priede", sep=", ")
+print("bērzs", "osis", "egle", "priede", sep=", ", end=".")
+print("Māris")
+
+print("bērzs", "osis", "egle", "priede", sep=", ", end=".\n") # Escape simbols \
+print("Māris")
+
+print("lacis\tkakis\tsuns\tvilks")
+
+# Šis ir komentārs un tas tiek ignorēts (viss aiz # tiek ignorēts)
+
+'''
+Šis ir garš komentārs
+pa vairākām
+rindiņām
+'''
+
+"""
+Šis ir garš komentārs
+pa vairākām
+rindiņām
+"""
+
+print("Gribu ātrāk siltu laiku!")
+print("Gribu ātrāk siltu laiku!")
+print("Gribu ātrāk siltu laiku!")
+print("Gribu ātrāk siltu laiku!")
+
+# print("Gribu ātrāk siltu laiku!") # Aizkomentēt/atkomentēt ar Ctrl + /
+# print("Gribu ātrāk siltu laiku!")
+# print("Gribu ātrāk siltu laiku!")
+# print("Gribu ātrāk siltu laiku!")
+
+print("Gribu ātrāk siltu laiku!")
+print("Gribu ātrāk siltu laiku!")
+print("Gribu ātrāk siltu laiku!")
---
+++ uzdevumi_print.md
@@ -0,0 +1,31 @@
+# Komandas print() uzdevumi
+## 1. uzdevums
+Izdrukāt savu vārdu.
+
+## 2. uzdevums
+Izdrukāt dzejoļa/dziesmas četrrindi, tā, lai katra rindiņa būtu ar 2 atstarpēm tālāk (no kreisās puses), kā iepriekšējā.
+
+Pirmā rindiņa
+  Otrā rindiņa
+Trešā rindiņa
+  Ceturtā rindiņa
+
+## 3. uzdeuvms
+Izdrukāt to pašu četrrindi izmantojot vienu print komandu.
+Pirmā rindiņa
+  Otrā rindiņa
+Trešā rindiņa
+  Ceturtā rindiņa
+
+## 4. uzdevums
+Izdrukāt eglīti ar zvaigznītēm.
+   *
+  ***
+ *****
+   *
+
+## 5. uzdevums
+Izdrukāt divus trīsstūrus viens otram blakus (augstumā 3)
+  *     *
+ ***   ***
+***** *****
