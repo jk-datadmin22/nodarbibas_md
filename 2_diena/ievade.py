3dd23c529deef6767c2ed4dc261b814f651d4dc7
Maris Danne <maris.danne@gmail.com>
2022-04-28 19:24:44 +0300

    ievade

---
+++ 2_diena/ievade.py
@@ -0,0 +1,22 @@
+# teksts = input()
+# print(teksts)
+
+# vards = input("Ievadi savu vārdu:")
+# print(vards)
+
+sk1 = input("Ievadi skaitli 1:") # sk1 = '1'
+sk2 = input("Ievadi skaitli 2:")
+print("Summa ir", sk1 + sk2)
+print(type(sk1))
+print(type(sk2))
+
+# sk1 = "5"
+# sk2 = "7"
+# print("Summa", sk1 + sk2)
+print("===================================")
+
+sk1 = int(input("Ievadi skaitli 1:"))
+sk2 = int(input("Ievadi skaitli 2:"))
+print("Summa ir", sk1 + sk2)
+print(type(sk1))
+print(type(sk2))
