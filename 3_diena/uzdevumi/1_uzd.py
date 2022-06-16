8fae7f0cf1cdc1235e6fca9ce3ff8fe1de23aa68
Maris Danne <maris.danne@gmail.com>
2022-06-16 18:30:02 +0300

    1 uzd risinajums

---
+++ 3_diena/uzdevumi/1_uzd.py
@@ -0,0 +1,11 @@
+# Uzrakstiet programmu, kas prasa lietotājam ievadīt savu vārdu, 
+# ja tas ievada vārdu, Bond, tad izdrukājiet uz ekrāna "Esi sveicināts 007", 
+# ja cits tad "Esi sveicināts, VARDS", kur VARDS ir lietotāja ievadītais vārds.
+
+vards = input("Ievadi savu vārdu:")
+
+if vards == "Bond":
+    print("Esi sveicināts, 007")
+else:
+    print("Esi sveicināts,", vards)
+
