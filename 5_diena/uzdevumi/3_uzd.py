a348070c70aaf82a750864000bcd50c9c9ba8193
Maris Danne <maris.danne@gmail.com>
2022-06-18 15:04:30 +0300

    uzdevums

---
+++ 5_diena/uzdevumi/3_uzd.py
@@ -0,0 +1,27 @@
+# Izveido programmu, kur lietotājam ir jāuzmin datora iedomātais 
+# skaitlis (no 1 līdz 20). Lietotājs min tik ilgi, 
+# kamēr ir atminējis skaitli, beigās parāda tekstu "Apsveicam ar uzvaru!" 
+# un rezultātu (minējuma skaitu).
+import random
+
+skaitlis = random.randint(1, 20) # datora iedomātais skaitlis
+meginajumi = 0
+
+while True:
+    minejums = int(input("Ievadi skaitli, kuru gribi uzminēt (0 - iziet):"))
+    meginajumi = meginajumi + 1
+
+    if minejums == 0:
+        print("Man žēl, ka tu padevies!")
+        break
+
+    if minejums == skaitlis:
+        print("Apsveicam ar uzvaru!")
+        print("Lai uzvarētu tev tas prasīja", meginajumi, "mēģinājumus.")
+        break
+    else:
+        if minejums > skaitlis:
+            print("Tavs minējums ir par augstu!")
+        else:
+            print("Tavs minējums ir par zemu.")
+        print("Mēģini vēlreiz ...")
--- 5_diena/uzdevumi/cikli.md
+++ 5_diena/uzdevumi/cikli.md
@@ -6,5 +6,8 @@ Aprēķināt no visiem ievadītajiem pāra skaitļiem vidējo aritmētisko. (0- 
 ## 2. uzdevums
 Izveidot programmu, kas prasa lietotājam ievadīt vairākus unikālus skaitļus (ja skaitlis jau ir iepriekš ievadīts, tad to neiekļauj) - ievade beidzas, kad lietotājs ir ievadījis 0. Izvadīt visus skaitļus un lielāko ievadīto skaitli, un mazāko ievadīto skaitli.
 
+if sk in saraksts # pārbada vai sarakstā ir sk
+if sk not in saraksts # pārbauda vai nav sarakstā
+
 ## 3. uzdevums
 Izveido programmu, kur lietotājam ir jāuzmin datora iedomātais skaitlis (no 1 līdz 20). Lietotājs min tik ilgi, kamēr ir atminējis skaitli, beigās parāda tekstu "Apsveicam ar uzvaru!" un rezultātu (minējuma skaitu).
