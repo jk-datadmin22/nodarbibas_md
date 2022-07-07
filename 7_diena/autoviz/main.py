ebcf53dbc41ddc11bca92d63dee5955af47c17cc
Maris Danne <maris.danne@gmail.com>
2022-07-07 18:30:35 +0300

    update

---
+++ 7_diena/autoviz/main.py
@@ -0,0 +1,18 @@
+from autoviz.AutoViz_Class import AutoViz_Class
+
+AV = AutoViz_Class()
+
+filename = "titanic_1.csv"
+
+data = AV.AutoViz(
+    filename,
+    sep=",",
+    depVar="",
+    dfte=None,
+    header=0,
+    verbose=2,
+    lowess=False,
+    chart_format="svg",
+    max_rows_analyzed=150000,
+    max_cols_analyzed=30 
+)
